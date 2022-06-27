af839831c73cfe3b0b94b7458a20dc32352a35de
Matheus C. Silva <matheuscarvas@gmail.com>
2022-06-27 16:30:01 -0300

    using postegres instead of sqlite

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./quiz-builder.db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config('POSTGRES_USER')}:{config('POSTGRES_PASS')}@localhost:5433/{config('POSTGRES_DB')}"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./quiz-builder.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

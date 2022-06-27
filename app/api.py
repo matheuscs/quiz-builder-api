6e31a8455cf02982d1208a3b1ca905df7e2c9086
Matheus C. Silva <matheuscarvas@gmail.com>
2022-06-27 16:30:38 -0300

    fixing cruds

--- app/api.py
+++ app/api.py
@@ -1,14 +1,12 @@
+from app.db import crud
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.auth.auth_bearer import verify_password
 from app.auth.auth_handler import create_access_token, decode_jwt, \
     credentials_exception
 from app.db import models, schemas
-from app.db.crud import get_user_by_email, get_quizes, get_users, create_user, \
-    create_user_quiz, get_questions, create_quiz_question, get_answers, \
-    create_question_answer
 from app.db.database import engine, SessionLocal
 from app.db.schemas import Token
 
@@ -30,109 +28,134 @@ def get_db():
 async def login_for_access_token(
         db: Session = Depends(get_db),
         form_data: OAuth2PasswordRequestForm = Depends()):
-    user = get_user_by_email(db, form_data.username)
+    user = crud.get_user_by_email(db, form_data.username)
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise credentials_exception
     access_token = create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/secure_hello/{name}")
-async def hello_name(name: str, token: str = Depends(oauth2_scheme)):
+@app.get("/alive")
+def i_am_alive():
+    return {"msg": "i'm alive"}
+
+
+@app.get("/alive_secure")
+async def hello_name(token: str = Depends(oauth2_scheme)):
     decode_jwt(token)
-    return {"message": f"Hello {name}"}
+    return {"msg": "i'm alive and secure"}
 
 
-@app.get("/")
-def root():
-    return {"message": "i'm alive"}
+
+# USERS
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate,
+                db: Session = Depends(get_db),
+                token: str = Depends(oauth2_scheme)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def get_user_by_id(user_id: int,
+                   db: Session = Depends(get_db),
+                   token: str = Depends(oauth2_scheme)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.get("/users/{email}")
+def get_user_by_email(email,
+                      db: Session = Depends(get_db),
+                      token: str = Depends(oauth2_scheme)):
+    return crud.get_user_by_email(db, email)
 
 
 @app.get("/users")
-def getusers(db: Session = Depends(get_db)):
-    return get_users(db)
+def get_all_users(db: Session = Depends(get_db),
+                  token: str = Depends(oauth2_scheme)):
+    return crud.get_users(db)
 
 
-@app.get("/users/<email>")
-def getuser(email, db: Session = Depends(get_db)):
-    return get_user_by_email(db, email)
-
-
-@app.get("/quizes")
-def getquizes(db: Session = Depends(get_db)):
-    return get_quizes(db)
-
-
+# QUIZES
 @app.post("/users/{user_id}/quiz/", response_model=schemas.Quiz)
 def create_quiz_for_user(
         user_id: int,
         quiz: schemas.QuizCreate,
-        db: Session = Depends(get_db)
-):
-    return create_user_quiz(db=db, quiz=quiz, user_id=user_id)
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)):
+    return crud.create_user_quiz(db=db, quiz=quiz, user_id=user_id)
 
 
-@app.get("/questions")
-def getquestions(db: Session = Depends(get_db)):
-    return get_questions(db)
+@app.get("/quiz/{quiz_id}", response_model=schemas.Quiz)
+def get_quiz_by_id(quiz_id: int,
+                   db: Session = Depends(get_db),
+                   token: str = Depends(oauth2_scheme)):
+    db_quiz = crud.get_quiz(db, quiz_id=quiz_id)
+    if db_quiz is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return db_quiz
 
 
+@app.get("/quizes")
+def get_all_quizes(db: Session = Depends(get_db),
+                   token: str = Depends(oauth2_scheme)):
+    return crud.get_quizes(db)
+
+
+# QUESTIONS
 @app.post("/quiz/{quiz_id}/question/", response_model=schemas.Question)
 def create_question_for_quiz(
         quiz_id: int,
         question: schemas.QuestionCreate,
-        db: Session = Depends(get_db)
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
 ):
-    return create_quiz_question(db=db, question=question, quiz_id=quiz_id)
+    db_quiz = get_quiz_by_id(quiz_id, db)
+    if len(db_quiz.questions) >= 10:
+        raise HTTPException(status_code=403,
+                            detail="Maximum questions for a quiz reached: 10")
+    return crud.create_quiz_question(db=db, question=question, quiz_id=quiz_id)
 
 
-@app.get("/answers")
-def getanswers(db: Session = Depends(get_db)):
-    return get_answers(db)
+@app.get("/question/{question_id}", response_model=schemas.Question)
+def get_question_by_id(question_id: int,
+                       db: Session = Depends(get_db),
+                       token: str = Depends(oauth2_scheme)):
+    db_question = crud.get_question(db, question_id=question_id)
+    if db_question is None:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return db_question
 
 
+@app.get("/questions")
+def get_all_questions(db: Session = Depends(get_db),
+                      token: str = Depends(oauth2_scheme)):
+    return crud.get_questions(db)
+
+
+# ANSWERS
 @app.post("/question/{question_id}/answer/", response_model=schemas.Answer)
 def create_anwswer_for_question(
         question_id: int,
         answer: schemas.AnswerCreate,
-        db: Session = Depends(get_db)
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
 ):
-    return create_question_answer(db=db, answer=answer,
-                                  question_id=question_id)
+    db_question = get_question_by_id(question_id, db)
+    print(f'{db_question.answers}')
+    if len(db_question.answers) >= 5:
+        raise HTTPException(status_code=403,
+                            detail="Maximum answers for a question reached: 5")
+    return crud.create_question_answer(db=db, answer=answer,
+                                       question_id=question_id)
 
 
-#
-# # DB
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
-#
-#
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = crud.get_users(db, skip=skip, limit=limit)
-#     return users
-#
-#
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
-#
-#
-# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-# def create_item_for_user(
-#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-# ):
-#     return crud.create_user_item(db=db, item=item, user_id=user_id)
-#
-#
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
+@app.get("/answers")
+def get_all_answers(db: Session = Depends(get_db),
+                    token: str = Depends(oauth2_scheme)):
+    return crud.get_answers(db)
--- app/db/crud.py
+++ app/db/crud.py
@@ -4,18 +4,7 @@ from app.auth.auth_bearer import get_password_hash
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
-
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
-
-
+# USERS
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
         email=user.email,
@@ -27,10 +16,23 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 
-def get_quizes(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Quiz).offset(skip).limit(limit).all()
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+# def get_user_by_quiz(db: Session, email: str):
+#     return db.query(models.User).filter(models.User.quizes == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+# QUIZES
 def create_user_quiz(db: Session, quiz: schemas.QuizCreate, user_id: int):
     db_quiz = models.Quiz(**quiz.dict(), user_id=user_id)
     db.add(db_quiz)
@@ -39,12 +41,36 @@ def create_user_quiz(db: Session, quiz: schemas.QuizCreate, user_id: int):
     return db_quiz
 
 
+def get_quiz(db: Session, quiz_id: int):
+    return db.query(models.Quiz).filter(models.Quiz.id == quiz_id).first()
+
+
+def get_quizes(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Quiz).offset(skip).limit(limit).all()
+
+
+# QUESTION
+def create_quiz_question(db: Session, question: schemas.QuestionCreate, quiz_id: int):
+    db_quiz = models.Question(**question.dict(), quiz_id=quiz_id)
+    db.add(db_quiz)
+    db.commit()
+    db.refresh(db_quiz)
+    return db_quiz
+
+
+def get_question(db: Session, question_id: int):
+    return db.query(models.Question).filter(
+        models.Question.id == question_id
+    ).first()
+
+
 def get_questions(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Question).offset(skip).limit(limit).all()
 
 
-def create_quiz_question(db: Session, question: schemas.QuestionCreate, quiz_id: int):
-    db_quiz = models.Question(**question.dict(), quiz_id=quiz_id)
+# ANSWERS
+def create_question_answer(db: Session, answer: schemas.AnswerCreate, question_id: int):
+    db_quiz = models.Answer(**answer.dict(), question_id=question_id)
     db.add(db_quiz)
     db.commit()
     db.refresh(db_quiz)
@@ -54,22 +80,3 @@ def create_quiz_question(db: Session, question: schemas.QuestionCreate, quiz_id:
 def get_answers(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Answer).offset(skip).limit(limit).all()
 
-
-def create_question_answer(db: Session, answer: schemas.AnswerCreate, question_id: int):
-    db_quiz = models.Answer(**answer.dict(), question_id=question_id)
-    db.add(db_quiz)
-    db.commit()
-    db.refresh(db_quiz)
-    return db_quiz
-
-
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
-
-
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
